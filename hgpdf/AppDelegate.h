efd395c0757d173fe9e679d06e038fe1a634f0e9
harry <harry@harrytekiMacBook.local>
2013-07-31 16:39:30 +0800

    Initial Commit

---
+++ hgpdf/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  hgpdf
+//
+//  Created by harry on 13-7-31.
+//  Copyright (c) 2013年 harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ hgpdf/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hgpdf
+//
+//  Created by harry on 13-7-31.
+//  Copyright (c) 2013年 harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
